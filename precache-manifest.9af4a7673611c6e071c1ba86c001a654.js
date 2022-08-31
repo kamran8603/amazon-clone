0524406b5a0a36d4844f970407d38f070ade0c04
kamran8603 <haiderkamran2@gmail.com>
2022-08-31 18:34:14 +0530

    Updates

---
+++ precache-manifest.9af4a7673611c6e071c1ba86c001a654.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ff5350eedd78f2820a4e24c8832281",
+    "url": "/newclonne/index.html"
+  },
+  {
+    "revision": "b9df29014df374627407",
+    "url": "/newclonne/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "3aaa270aea4005b239e7",
+    "url": "/newclonne/static/js/2.d302e61b.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/newclonne/static/js/2.d302e61b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9df29014df374627407",
+    "url": "/newclonne/static/js/main.b1cd4e35.chunk.js"
+  },
+  {
+    "revision": "125de15b6b238d8f06d2",
+    "url": "/newclonne/static/js/runtime-main.f7efa123.js"
+  }
+]);
